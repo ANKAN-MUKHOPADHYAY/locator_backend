1d8f550a91999871051671be2efdda5cc6d2de20
Ankan Mukhopadhyay <ankanmukhopadhyay@Ankans-MacBook-Pro.local>
2018-05-24 19:51:45 +0530

    Added code to update institute info

--- routes/institute/instituteOperations.js
+++ routes/institute/instituteOperations.js
@@ -276,4 +276,33 @@ router.get('/getInstituteInformation/:instid', function(req,res){
 	});
 });
 
+router.post('/updateInstituteBasicInfo', function(req,res){
+	var chkQry = "SELECT * FROM institute_registration WHERE ??=?";
+	var cheQryData = ['id',req.body.instid];
+	chkQry = mysql.format(chkQry,cheQryData);
+	console.log()
+	connection.query(chkQry,function(errr,results){
+		console.log(results);
+		var updateQry;
+		var updateData;
+		if(results.length==1){
+			if(req.body.hasOwnProperty('inst_about')){
+				updateQry = 'UPDATE institute_registration SET ??=?, ??=? WHERE ??=?';
+				updateData = ['inst_about',req.body.inst_about,'inst_desc',req.body.inst_desc,'id',req.body.instid];
+				updateQry = mysql.format(updateQry, updateData);
+			} else {
+				updateQry = 'UPDATE institute_registration SET ??=?,??=?,??=?,??=?,??=? WHERE ??=?';
+				updateData = ['inst_contact',req.body.inst_contact,'inst_altcontact',req.body.inst_altcontact,'inst_email',req.body.inst_email,'inst_founder',req.body.inst_founder,'inst_founded_date',req.body.inst_founded_date,'id', req.body.instid];
+				updateQry = mysql.format(updateQry, updateData);
+			}
+			console.log(updateQry);
+			connection.query(updateQry,function(er,result){
+				res.json({status: true, message: 'Institute info updated Successfully'});
+			});
+		} else {
+			res.json({status: false,message: 'There is some problem'});
+		}
+	});
+});
+
 module.exports = router;
